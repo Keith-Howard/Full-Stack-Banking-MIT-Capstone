20b28993c6b8c2b1dde46a13fa229e7834465dea
keith howard <keithwhoward88@gmail.com>
2022-10-13 15:49:56 -0400

    update links

--- public/home.js
+++ public/home.js
@@ -8,7 +8,6 @@ function Home() {
             body={
                 <>
                 <h5 className="card-title">Welcome to your digital banking experience.</h5>
-                <p className="card-text">You can move around using the navigation bar.</p>
                 <img src="./bank.png" className="img-fluid" alt="Responsive"></img>
                 </>
             }
--- public/navbar.js
+++ public/navbar.js
@@ -2,37 +2,37 @@ function NavBar(){
     return(
   
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a id="home" className="navbar-brand" style={{visibility: 'visible'}} href="#/">First National Bank</a>
+        <a id="home" className="navbar-brand" style={{display: 'inline'}} href="#/">First National Bank</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a id="createAccount" className="nav-link" style={{visibility: 'visible'}} href="#/createAccount/">Create Account</a>
+              <a id="createAccount" className="nav-link" style={{display: 'inline'}} href="#/createAccount/">Create Account</a>
             </li>
             <li className="nav-item">
-              <a id="login" className="nav-link" style={{visibility: 'visible'}} href="#/login/">Login</a>
+              <a id="login" className="nav-link" style={{display: 'inline'}} href="#/login/">Login</a>
             </li>
               <li className="nav-item">
-                <a id="deposit" className="nav-link" style={{visibility: 'hidden'}} href="#/deposit/">Deposit</a>
+                <a id="deposit" className="nav-link" style={{display: 'none'}} href="#/deposit/">Deposit</a>
               </li>
               <li className="nav-item">
-                <a id="withdraw" className="nav-link" style={{visibility: 'hidden'}} href="#/withdraw/">Withdraw</a>
+                <a id="withdraw" className="nav-link" style={{display: 'none'}} href="#/withdraw/">Withdraw</a>
               </li>
               <li className="nav-item">
-                <a id="balance" className="nav-link" style={{visibility: 'hidden'}} href="#/balance/">Balance</a>
+                <a id="balance" className="nav-link" style={{display: 'none'}} href="#/balance/">Balance</a>
               </li>
               <li className="nav-item">
-                <a id="allData" className="nav-link" style={{visibility: 'hidden'}} href="#/alldata/">All Data</a>
+                <a id="allData" className="nav-link" style={{display: 'none'}} href="#/alldata/">All Data</a>
               </li>
               <li className="nav-item">
-                <a id="logout" className="nav-link" style={{visibility: 'hidden'}} href="#/logout/">Log Out</a>
+                <a id="logout" className="nav-link" style={{display: 'none'}} href="#/logout/">Log Out</a>
               </li>
           </ul>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a id="userName" className="nav-link" style={{visibility: 'hidden', fontWeight: 'bold'}}></a>
+              <a id="userName" className="nav-link" style={{display: 'none', fontWeight: 'bold'}}></a>
             </li> 
           </ul>
         </div>
